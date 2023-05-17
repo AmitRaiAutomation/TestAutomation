a3b2c893f745c48bc0ff6da787ee642166f5dcc6
Amit Rai <amit.rai2@tesco.com>
2023-05-17 14:32:09 +0530

    Initial commit
    Initial commit

---
+++ src/test/java/Demo.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Demo {
+
+    WebDriver driver;
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("automationName" , "UiAutomator2");
+        caps.setCapability("platformName" , "Android");
+        caps.setCapability("platformVersion" , "5");
+        caps.setCapability("deviceName" , "Test1");
+        caps.setCapability("app" , "/Users/amit.rai/Documents/app/Tesco_online.apk");
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
+
+    @Test
+    public void test_Appium() throws MalformedURLException, InterruptedException {
+        String message = "Hello GitHub Actions";
+//        WebElement messageTxt = driver.findElement(By.id("my_text_field"));
+//        messageTxt.sendKeys(message);
+//        System.out.println(messageTxt.getText());
+//        Assert.assertEquals(message,messageTxt.getText());
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
